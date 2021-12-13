5e0860b877bb75a511b65482af05050873b472eb
Tahir <abbasovtahir617@gmail.com>
2021-12-13 17:54:16 +0100

    arrays based tasks

---
+++ Algorithms/tasks.js
@@ -0,0 +1,18 @@
+//1.Parametr olaraq ədəd qəbul edib o ədədin kvadratını return edən method yazın
+
+/*function squareNum (num) {
+    return num * num
+}
+console.log(squareNum(2))
+*/
+
+
+//2.Parametr olaraq string qəbul edib verilən stringin hərflərini böyük hərfə çevirərək return edən method yazın
+
+/*function upperStr(text) {
+    let str = 'salam'
+    let result = str.toUpperCase()
+    console.log(result)
+}    
+upperStr()
+*/
