6b906daf30297acfe9c820a0dd3f03045821e1db
Tahir <abbasovtahir617@gmail.com>
2021-12-17 17:41:53 +0100

    today's task tried to solve

--- Algorithms/Js-DOM/style.js
+++ Algorithms/Js-DOM/style.js
@@ -1,10 +1,10 @@
 
 function changeImage(x){
+    //a deyishenine butun small-img leri massiv sheklinde yaziriq
     let a = document.querySelectorAll('.small-image img')
+    //b deyishenine cari sheklin src ni yaziriq
+    //cari shekil [x-1] indeksi ile teyin olunur
     let b = a[x-1].getAttribute('src')
+    //big-image deki shekilde oxudugumuz src teyin edirik.  
     document.querySelector('.big-image img').setAttribute('src', b)
-    
-   
-   
-
 }
