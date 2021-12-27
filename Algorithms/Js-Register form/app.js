50e5a15a86f614453e033494da296900de89e532
Tahir <abbasovtahir617@gmail.com>
2021-12-27 17:43:46 +0100

    registration form almost ready)

--- Algorithms/Js-Register form/app.js
+++ Algorithms/Js-Register form/app.js
@@ -1,2 +1,21 @@
-var x = document.getElementsByClassName('mail').value;
-x.oninvalid = alert('start with 2');
+function check(){
+    let mail=document.getElementById('mail').value; //checking email addres if that contains of '@'
+    
+    if (mail.indexOf('@')<0){
+        alert('You must add "@"')
+    }else {
+        alert('Correct')
+    }
+    let phone = document.getElementById('phone').value; // checking number if that starts with '+994' and consists of 13 figures
+    if (phone.includes('+994') && phone.length == '13') {
+        document.write('Please add "+994" at begining and number must consist of 13')
+    }else{
+        alert('Correct')
+    }
+
+    /*let birthday = document.getElementById('birthday').value; Find out if entered birthday date not elder than 15 years
+    let year = new Date().getFullYear()
+    if ('#'){
+        alert('small')
+    }*/
+}
