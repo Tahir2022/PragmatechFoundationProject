c0f62a4779e3d57e49128945f6f7754176d638ca
Tahir <abbasovtahir617@gmail.com>
2022-01-11 20:59:26 +0100

    Tasks, based on strings methods, got solved

---
+++ Python_Fundamentals/Date11/01/app.py
@@ -0,0 +1,60 @@
+#1) 3 input daxil edin və daxil edilən ədələrin fərqini tapın
+#num1 = input('Please add first number: ')
+#num2 = input('Please add second number: ')
+#num3 = input('Please add thrid number: ')
+
+#print(int(num1) - int(num2) - int(num3))
+
+
+#2) word = 'Python'. Verilen stringin uzerinde azı 5 dənə string method tətbiq edin
+#word = 'Python'
+
+#print(word.upper()) # upper()
+#print(word.lower()) #lower()
+#print(word.replace("y", "i")) #replace()
+#print(word.center(10, '*')) #center()
+#print(word.split("t")) #split()
+#print(word[1:4]) #slicing str
+#print(word.casefold()) #casefold()
+
+
+
+#3) math kitabxanasından istifadə edərək inputdan hələn ədədin kubunu tapın
+
+#import math
+
+#number = int(input('Pls add any number:'))
+#x = math.pow(number, 2)
+
+#print(x)
+
+
+
+#4) inputdan 3 ədəd götürün və o ədələrin kvadratını bir listə əlavə edin
+
+#import math
+
+#num1 = int(input('Pls add number: '))
+#num2 = int(input('Pls add number: '))
+#num3 = int(input('Pls add number:'))
+
+#a = math.pow(num1, 2)
+#b = math.pow(num2, 2)
+#c = math.pow(num3, 2)
+#array = []
+#result = [a,b,c]
+#array.extend(result)
+#print(array)
+
+
+# Təsadüfi rəqəmlərdən ibarət olan listin rəqəmlərini kiçikdən böyüyə düzün
+
+#num1 = int(input('Pls add any number: '))
+#num2 = int(input('Pls add any number: '))
+#num3 = int(input('Pls add any number: '))
+#num4 = int(input('Pls add any number: '))
+
+#list = [num1, num2, num3, num4]
+#list.sort()
+
+#print(list)
