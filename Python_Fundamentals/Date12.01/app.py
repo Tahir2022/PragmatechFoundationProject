83418018c03c07df5b3be6300ef565255506e5cb
Tahir <abbasovtahir617@gmail.com>
2022-01-12 17:59:28 +0100

    some of tasks got solved

---
+++ Python_Fundamentals/Date12.01/app.py
@@ -0,0 +1,76 @@
+# 1. Bir mesajı dəyişəndə saxlayın və sonra mesajı çapa verin
+
+#txt = 'Today is Pi day'
+#print(txt)
+
+#2. ad və soyad dəyişkənləri yaradın və onlara istədiyiniz kiçik hərflərdən ibarət dəyər verin. Sonra tam_ad adlı dəyərdə ad və soyadın ilk hərflərini böyük şəkildə çapa verərək həmin şəxsə Salam verin.
+#Nümunə: Salam, Arif Dadaşov! (Ekrana bu yazı çıxsın)
+
+#ad = 'elon'
+#soyad = 'musk'
+#tam_ad = f"Salam, {ad.capitalize()} {soyad.capitalize()}!"
+
+#print(tam_ad)
+
+#3. sep parametrindən istifadə edərək 4 müxtəlif şəhər adını * işarəsi ilə ayırın
+
+#print('Baku', 'Moscow', 'Istanbul', 'Berlin', sep='*')
+
+#4. “Macarıstan” sözünü tərsinə çapa verin
+
+#country = 'Macarıstan'[::-1]
+
+#print(country)
+
+
+#5. Bir sətir koddan istifadə edərək aşağıdakı yazını göründüyü kimi çapa verin. Languages: Python C JavaScript
+
+#print('Python', 'C', 'Javascript', sep=' ')
+
+#6.Istenilen bir mətnin tam yarısını çap edin
+
+#txt = 'I will try print a half of this text'
+
+#print(txt[0:18])
+
+
+#7. x = 10, y = 55 “and”-dən istifadə edərək x və y müqayisə edərək boolen dəyərləri çapa verin.
+
+#x = 10
+#y = 55
+
+#print(x < y and y > x)
+
+
+#8. inputdan boshluqla ayrilmish iki eded daxil edin. Birinci ededi ikinci eded qeder quvvete yukseldin ve ashagidaki kimi ekrana yazdirin (f stringden istifade ederek)
+
+#Ededleri daxil edin: 2 5
+
+#Netice: 2 üstü 5 32-dir.
+
+#import math
+
+#a = int(input(' '))
+#b = int(input(' '))
+#result =pow(a, b)
+
+#txt = f'Netice: {a} ustu {b} beraberdir -  {result}'
+#print(txt)
+
+#65 ədədinin 22 ədədinə olan qalığı ilə nisbətinin hasilini çapa verin.
+
+#first_num = 44
+#second_num = 22
+#rem =first_num % second_num
+#div = first_num / second_num
+#print(rem * div)
+
+
+#18. y = “4.92” stringini integere cast edin.
+
+#y = '4.92'
+#x = int('4.92')
+
+#print(x)
+
+
