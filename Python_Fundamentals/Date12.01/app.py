d0e68e636f01f7326f653ac244464de586314f5f
Tahir <abbasovtahir617@gmail.com>
2022-01-13 17:33:03 +0100

    solved, except the last one

--- Python_Fundamentals/Date12.01/app.py
+++ Python_Fundamentals/Date12.01/app.py
@@ -25,7 +25,7 @@
 
 #5. Bir sətir koddan istifadə edərək aşağıdakı yazını göründüyü kimi çapa verin. Languages: Python C JavaScript
 
-#print('Python', 'C', 'Javascript', sep=' ')
+#print('Python', 'C', 'Javascript', sep='\n')
 
 #6.Istenilen bir mətnin tam yarısını çap edin
 
@@ -50,20 +50,71 @@
 
 #import math
 
-#a = int(input(' '))
-#b = int(input(' '))
-#result =pow(a, b)
+#a = input('Ededleri daxil edin: ')
+#b = a.split('ustu')
+#result = int(b[0] ) **int(b[1])
 
-#txt = f'Netice: {a} ustu {b} beraberdir -  {result}'
+#txt = f'Netice: {b[0]} ustu {b[1]} beraberdir -  {result}'
 #print(txt)
 
-#65 ədədinin 22 ədədinə olan qalığı ilə nisbətinin hasilini çapa verin.
+# 10. 65 ədədinin 22 ədədinə olan qalığı ilə nisbətinin hasilini çapa verin.
 
-#first_num = 44
+#first_num = 65
 #second_num = 22
 #rem =first_num % second_num
 #div = first_num / second_num
 #print(rem * div)
+
+#11. x-ə istənilən bir ədəd mənimsədin, sonra isə şərt verərək yoxlayın. X 10-dan böyükdürsə və cüt ədədirsə, ekrana “OKAY” yazılsın, əgər yuxarıdakı iki şərtdən biri ödənirsə “NOT OKAY” yazılsın, heç bir şərt ödənməzsə, “BAD” yazılsın
+
+#x = 2
+#if (x > 10) and (x % 2==0):
+#    print('OKAY')
+#elif (x > 10) or (x % 2 !=0):
+#    print('NOT OKAY')
+#else:
+#    print('BAD') 
+
+
+#12. iki ədəd götürüb dəyişkənlərə mənimsədin. Əgər ədələrin fərqi bir-birlərinə olan nisbətin tam hissəsindən böyükdürsə, ekrana “Greater”, bərabərdirsə, “EQUAL” yox əgər kiçikdirsə, “SMALLER” yazılsın.
+
+# num1 = 100
+# num2= 10
+# subs = num1 - num2
+# div = int(float(num1 / num2))
+
+# if (subs > div):
+#     print('GREATER')
+# elif (subs == div):
+#     print('EQUAL')
+# elif (subs < div):
+#     print('SMALLER')
+
+#13. String data tipi yaradın və dəyərini 5.567-yə bərabər edin. Sonra bu dəyişkənin dəyərin 10- luqlara qədər yuvarlaqlaşdırın.
+
+# import math
+
+# string = '5.567'
+# convert_str = float(string)
+# convert_str = round(convert_str, 1)
+
+# print(convert_str)
+
+#14. my_string = ‘f4.3989ts’. my_stringin ədədə bərabər olan hissəsin ilə özündən sonra gələn ən kiçik tam ədədə olan qüvvətini tapın
+
+#my_string = 'f4.3989ts'
+#b = float(my_string[1:7])
+#a = int(b) + 1
+#print(b **a)
+
+
+#17. x = “5.89” stringinin tam hissəsinin kubunu tapın.
+
+# import math
+
+# x = '12.63'
+# y = int(float(x))
+# print(pow(y, 3))
 
 
 #18. y = “4.92” stringini integere cast edin.
@@ -72,5 +123,12 @@
 #x = int('4.92')
 
 #print(x)
+
+
+
+
+
+
+
 
 
---
+++ Python_Fundamentals/Date13.01/app.py
@@ -0,0 +1,82 @@
+#1. Kvadrat yaratmaq olarsa daxil edilən 4 ədəddən, ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini verin.
+
+# import math
+
+# def square ():
+#     a = int(input('Add pls first number: '))
+#     b = int(input('Add pls second number: '))
+#     c = int(input('Add pls thrid number: '))
+#     d = int(input('Add pls fourth number: '))
+
+#     if (a == b == c == d):
+#         print(pow(a, 2))
+#     else:
+#         print(a + b + c + d)
+
+# square()
+
+
+#2. Dörd ədəd daxil edin. Onlardan ən böyüyünü çapa verin.
+
+# def check_max():
+#     num1 = input('Add pls first number: ')
+#     num2 = input('Add pls second number: ')
+#     num3 = input('Add pls thrid number: ')
+#     num4 = input('Add pls fourth number: ')
+
+#     if (num1 > num2) and (num1 > num3) and (num1 > num4):
+#         print(num1)
+#     elif (num2 > num1) and (num2 > num3) and (num2 > num4):
+#         print(num2)
+#     elif (num3> num1) and (num3 > num2) and (num3 > num4):
+#         print(num3)
+#     elif (num4 > num1) and (num4 > num2) and (num4 > num3):
+#         print(num4)
+# check_max()
+
+#3. Proqram run olunanda, istifadəçiyə meyvələr menyusu təqdim olunsun. Userdən menyunuzdakı meyvələrdən birinin adını daxil etməsini tələb edin və ekrana meyvənin qiymətini yazdırın. (İstədiyiniz qiyməti yazdırın). Əgər menyuda olmayan meyvə daxil edilsə, ekrana error mesajı verin.
+
+# def find_fruits ():
+#     fruits = ['apple', 'cherry', 'watermelon', 'orange']
+#     print(fruits)
+
+#     a = input('Write any fruit: ')
+#     if a in fruits:
+#         print(input(f'{a} nin qiymetini yazin: '))
+#     elif a not in fruits:
+#         print('ERROR')
+
+# find_fruits()
+
+
+#4. Qeydiyyat formu düzəldin. İstifadəçi adını daxil etsin. Adın uzunlu 3-dən kiçik 11-dən böyük ola bilməz.Əgər adını düzgün daxil edərsə, soyadının daxil etməsini istəyin. Soyad 5 hərfdən kiçik, 15 hərfdən uzun olmasın. Əgər soyad düzgün daxil edilsə, Daha sonra doğulduğu ili daxil etsin. Doğum ilinin uzunluğu 4 simvoldan ibarət olmalıdır. Sonra email daxil etməsini tələb edin. Emailin uzunluğu 10 hərifdən qısa 22 hərfdən uzzun olmasın, tərkibində @gmail.com olsun və bu hissə daxil etdiyi emailin sonunda olsun. Ardınca parol axil etsin. Parol 6-13 simvol aralığında olmalıdır. Sonra parolu təsdiqləməyini tələb edin. Təsdiqlədiyi parol birinci yazdığı parol ilə eyni olmalıdır. Bütün bunlar düzgün daxil edilərsə, Qeydiyyat tamamlandı mesajı verilsin və istifadəçidən qeydiyyatın detallarına baxmaq istəyib-istəməməsi soruşulsun. Əgər hə desə, aşağıdakı görüntü çapa verilsin: (Qeyd: Yuxarıda sizin verdiyiniz şərtlərə uyğun istifadəçi input daxil etmsəsə, hər verdiyibiz şərtə uyğun error tipli mesaj verin. Məsələn doğum ilini 5 simvoldan ibarət daxil etsə, mesaj verilsin ki 4 simvol daxil edin. Yəni hər şərti müvafiq mesajlar ilə userə izah edin. ============================================= Ad: Murad Soyad: Əliyev Yaş: 22 Email: muradaliyev1996@gmail.com Parol: 123456789 ============================================= Əgər yox desə, Murad Əliyev, Uğurlar! Yazılsın.
+
+
+def login ():
+    name = input('Pls add your name: ')
+    if 3 < len(name) < 11:
+        surname = input('Pls add your surname: ')
+        if 5 < len(surname) < 15:
+            year = input('Pls add your bith year: ')
+            if len(year) == 4:
+                email = input('Pls add your mail addres: ')
+                if 10 < len(email) < 22:
+                    password = input('Pls add password: ')
+                    if 6 < len(password) < 13:
+                        submit_pass = input('Submit password again: ')
+                        if password == submit_pass:
+                            return
+    
+    print()
+
+login()
+
+
+
+
+
+
+
+
+
+
