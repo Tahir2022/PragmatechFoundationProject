5adcfa1dcd93fbefceac8fc33657276a079c2608
Tahir <abbasovtahir617@gmail.com>
2022-01-14 17:49:55 +0100

    solved, except #6

--- Python_Fundamentals/Date13.01/app.py
+++ Python_Fundamentals/Date13.01/app.py
@@ -52,24 +52,21 @@
 #4. Qeydiyyat formu düzəldin. İstifadəçi adını daxil etsin. Adın uzunlu 3-dən kiçik 11-dən böyük ola bilməz.Əgər adını düzgün daxil edərsə, soyadının daxil etməsini istəyin. Soyad 5 hərfdən kiçik, 15 hərfdən uzun olmasın. Əgər soyad düzgün daxil edilsə, Daha sonra doğulduğu ili daxil etsin. Doğum ilinin uzunluğu 4 simvoldan ibarət olmalıdır. Sonra email daxil etməsini tələb edin. Emailin uzunluğu 10 hərifdən qısa 22 hərfdən uzzun olmasın, tərkibində @gmail.com olsun və bu hissə daxil etdiyi emailin sonunda olsun. Ardınca parol axil etsin. Parol 6-13 simvol aralığında olmalıdır. Sonra parolu təsdiqləməyini tələb edin. Təsdiqlədiyi parol birinci yazdığı parol ilə eyni olmalıdır. Bütün bunlar düzgün daxil edilərsə, Qeydiyyat tamamlandı mesajı verilsin və istifadəçidən qeydiyyatın detallarına baxmaq istəyib-istəməməsi soruşulsun. Əgər hə desə, aşağıdakı görüntü çapa verilsin: (Qeyd: Yuxarıda sizin verdiyiniz şərtlərə uyğun istifadəçi input daxil etmsəsə, hər verdiyibiz şərtə uyğun error tipli mesaj verin. Məsələn doğum ilini 5 simvoldan ibarət daxil etsə, mesaj verilsin ki 4 simvol daxil edin. Yəni hər şərti müvafiq mesajlar ilə userə izah edin. ============================================= Ad: Murad Soyad: Əliyev Yaş: 22 Email: muradaliyev1996@gmail.com Parol: 123456789 ============================================= Əgər yox desə, Murad Əliyev, Uğurlar! Yazılsın.
 
 
+
+from os import name
+
+
 def login ():
     name = input('Pls add your name: ')
     if 3 < len(name) < 11:
         surname = input('Pls add your surname: ')
         if 5 < len(surname) < 15:
-            year = input('Pls add your bith year: ')
-            if len(year) == 4:
-                email = input('Pls add your mail addres: ')
-                if 10 < len(email) < 22:
-                    password = input('Pls add password: ')
-                    if 6 < len(password) < 13:
-                        submit_pass = input('Submit password again: ')
-                        if password == submit_pass:
-                            return
-    
-    print()
+            year = input('')
 
-login()
+    
+  
+
+
 
 
 
---
+++ Python_Fundamentals/Date14.01/app.py
@@ -0,0 +1,92 @@
+#1. Listin elementlerini toplayan alqoritm yazin. Sum funksiyasindan istifade etmeyin.
+
+# my_list = [1, 5, 100, 78]
+# total = 0
+# for i in my_list:
+#     total+=i
+# print(total)
+
+# x=[1,2,3,4,5]
+# sum=0
+# for s in range(0,len(x)):
+#    sum=sum+x[s]
+# print(sum)
+
+#2. Listin en boyuk elementini max funksiyasini istifade etmededen tapan alqoritm yazin.
+
+# my_list = [5, 6, 9, 10]
+# maximum = my_list[0]
+# for i in my_list:
+#     if i > maximum:
+#         maximum = i
+# print(maximum)
+
+
+#3. Listin en kicik elementini min funksiyasini istifade etmededen tapan alqoritm yazin.
+
+# my_list = [5,7,69,74,3]
+# minimum = my_list[0]
+# for i in my_list:
+#     if i < minimum:
+#         minimum = i
+# print(minimum)
+
+
+#4. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings. Sample List : ['abc', 'xyz', 'aba', '1221'] Expected Result : 2
+
+
+
+# my_list = ['abc', 'xyz', 'aba', '1221']
+
+# for i in my_list:
+#     if (len(i) > 2) and i[0] ==i[-1]:
+#         print(i)
+
+
+# 5. Write a Python program to check a list is empty or not.
+
+# my_list = [ ]
+# if len(my_list) == 0:
+#     print('It is empty!')
+
+#6. my_text = “Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.” my_text stringindeki sozler elifba sirasi ile duzub string formatinda ekrana yazdirin.
+
+import string
+
+my_text = '''Write a Python program to count the number of strings where the string length is 2 or more  and the first and last character are same from a given list of strings.'''
+say = 0
+new_txt = my_text.split(' ')
+for i in new_txt:
+    if (len(i) > 2) and i[0] == i[-1]:
+        print(i)
+        print(my_text.ascii_lowercase)
+
+
+
+#7. Write a Python program to select the odd items of a list
+
+
+# my_list = [2, 5, 8, 9, 99, 11]
+# for i in my_list:
+#     if (i % 2!=0):
+#         print('Odds are: ', i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
