b6927058436acf96abaf0f9b0b44324c646109f1
Tahir <abbasovtahir617@gmail.com>
2022-01-20 17:46:09 +0100

    tried to solve some

---
+++ Python_Fundamentals/Date19.01/app.py
@@ -0,0 +1,43 @@
+#1. Üç müxtəlif a, b, c ədədləri verilmişdir. Onlardan qiymətcə orta olanı tapın.
+
+# a = int(input('Add first number: '))
+# b = int(input('Add second number: '))
+# c = int(input('Add third number: '))
+# print(int((a + b + c)/3))
+
+#2. Proqramlaşdırma olimpiadasının ikinci turu bitdikdən sonra olimpiada iştirakçıları bu hadisəni qeyd etmək qərarına gəldilər. Bu məqsədlə düzbucaqlı formada bir böyük tort sifarış verildi. Bu zaman iştirakçıların ətrafına toplaşdıqları masa dairəvi idi. Təbii ki, onlarda sual yarandı: Düzbucaqlı tort masadan qırağa çıxmamaq şərti ilə masaya yerləşəcəkdirmi? Tortun ölçülərini və masanın radiusunu bilərək, bu sualı cavablandırmalısınız.
+
+# from cmath import pi
+# import math
+
+# def area_of_cake():
+#     r = int(input('Add radius: '))
+#     w = int(input('Add width: '))
+#     l = int(input('Add length: '))
+#     cake = w * l
+#     table = pi * r ** 2
+#     if cake < table:
+#         print('yes')
+#     else:
+#         print('no')
+
+# area_of_cake()
+
+
+#4. m natural ədədi n ədədinin o zaman bərabər böləni adlanır ki, n-nin m-ə bölünməsindən alınan tam və qalıq bərabər olsun. Verilmiş n natural ədədinə görə onun bərabər bölənlərinin sayını tapın.
+
+
+# def equal(any):
+#     # num = int(input('Add any number: '))
+#     i = 1
+#     global b
+#     b = 0
+#     while i < any:
+#         print(i)
+#         rem = any % i
+#         wh =int(any / i)
+#     if rem == wh:
+#         b +=1
+#     i+=1
+
+# equal(10)
