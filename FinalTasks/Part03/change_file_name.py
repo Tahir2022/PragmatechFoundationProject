b278b3b62668567af6bc5c333ece8b72f92b10d6
Tahir <abbasovtahir617@gmail.com>
2022-02-06 10:35:17 +0100

    part03 sual 04

---
+++ FinalTasks/Part03/change_file_name.py
@@ -0,0 +1,19 @@
+# import os    first one
+# from os import path
+
+# def main():
+#     if path.exists('tural.txt'):
+#         src = path.realpath('tural.txt')
+#     os.rename('tural.txt', 'sahib.txt')
+
+# main()
+
+
+# import os     second one
+# from os import rename
+# path = input()
+# old_name = input()
+# new_name = input()
+# old_name = path + '/' + old_name
+# new_name = path + '/' + new_name
+# rename(old_name, new_name)
